7ca823e5aa34e3f4058969330dc87c03237ae510
Adarsh <adarshadhu47167@gmail.com>
2024-02-10 15:04:24 +0530

    logo animation added in flutter logo - LoginScreenAnimation

--- lib/explicit_examples/login_animation.dart
+++ lib/explicit_examples/login_animation.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 
-class LoginScreenAnimation extends StatelessWidget {
+class LoginScreenAnimation extends StatefulWidget {
   const LoginScreenAnimation({super.key});
 
+  @override
+  State<LoginScreenAnimation> createState() => _LoginScreenAnimationState();
+}
+
+class _LoginScreenAnimationState extends State<LoginScreenAnimation> with SingleTickerProviderStateMixin{
+  late AnimationController logoController;
+  late Animation<double> logoAnimation;
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    logoController = AnimationController(vsync: this,duration:const Duration(milliseconds: 3500));
+    logoAnimation = Tween<double>(begin: 0,end: 1).animate(logoController);
+    logoController.forward();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,8 +27,11 @@ class LoginScreenAnimation extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const FlutterLogo(
-              size: 100.0,
+            FadeTransition(
+              opacity: logoAnimation,
+              child: const FlutterLogo(
+                size: 100.0,
+              ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
